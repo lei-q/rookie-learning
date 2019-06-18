1d8e34463a3da5ea72e26a064f460760355d88c1
leiqiang <leiq@zwsec.com>
2019-06-18 10:13:57 +0800

    init

---
+++ src/main/java/com/lay/rookie/rookielearning/RookieLearningApplication.java
@@ -0,0 +1,13 @@
+package com.lay.rookie.rookielearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookieLearningApplication.class, args);
+	}
+
+}
